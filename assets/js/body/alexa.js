acc571ffb83c3df160e92232eb474d4f9e88b3fe
wesley.chen <qkevin850609@gmail.com>
2021-10-30 02:51:08 +0800

    remove no use

--- assets/js/body/alexa.js
+++
@@ -1,6 +0,0 @@
-
-const app = {
-    __html: "<img src=\"https://certify.alexametrics.com/atrk.gif?account=nnVtx1ZfGe209b\" style=\"display:none\" height=\"1\" width=\"1\" alt=\"\" />"
-};
-
-export default app;
--- assets/js/body/gtm.js
+++
@@ -1,6 +0,0 @@
-
-const app = {
-    __html: "<iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-P3GP48H\"\nheight=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe>"
-};
-
-export default app;
