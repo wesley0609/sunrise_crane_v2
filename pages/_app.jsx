9bfafde39d95eb732476c49ae7a0ccb1bcd24610
wesley.chen <qkevin850609@gmail.com>
2021-10-30 10:18:12 +0800

    default head meta

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,6 +9,8 @@ import Head from "next/head";
 import store from "../redux/store.js";
 import plugins from "../plugins/index.js";
 
+import pkg from "../package.json";
+
 import GlobalStyle from "../assets/css/global.jsx";
 import Loading from "../components/loading/index.jsx";
 import Header from "../components/header/index.jsx";
@@ -41,7 +43,13 @@ const App = ({Component, pageProps, ...etc}) => {
     return (
         <Provider store={store}>
             <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+                <title key="title">{pkg.title}</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" key="viewport" />
+                <meta name="description" content={pkg.description} key="description" />
+                <meta property="og:url" content={pkg.siteUrl} key="og:url" />
+                <meta property="og:title" content={pkg.title} key="og:title" />
+                <meta property="og:description" content={pkg.description} key="og:description" />
+                <link rel="canonical" href={pkg.siteUrl} key="canonical" />
             </Head>
 
             <GlobalStyle {...pageProps} />
