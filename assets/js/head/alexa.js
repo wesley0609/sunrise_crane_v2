953834c2387d52e9f75ac26d5d1587549e2550a7
wesley.chen <qkevin850609@gmail.com>
2022-01-14 16:33:58 +0800

    es6

--- assets/js/head/alexa.js
+++ assets/js/head/alexa.js
@@ -1,6 +1,6 @@
 
 const app = {
-    __html: "_atrk_opts = { atrk_acct:\"nnVtx1ZfGe209b\", domain:\"sunrise-crane.com.tw\",dynamic: true};\n(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = \"https://certify-js.alexametrics.com/atrk.js\"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();"
+    __html: `_atrk_opts = { atrk_acct:"nnVtx1ZfGe209b", domain:"sunrise-crane.com.tw",dynamic: true};(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://certify-js.alexametrics.com/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();`
 };
 
 export default app;
