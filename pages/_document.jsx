20a28065e3ccead1445e1743d465125f9e88a3f6
wesley.chen <qkevin850609@gmail.com>
2022-03-09 17:19:48 +0800

    favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -19,7 +19,7 @@ export default class App extends Document{
                     <meta name="theme-color" content={theme["--black"]} />
                     <meta property="fb:app_id" content="3115592912095632" />
                     <link rel="manifest" href="/manifest.json" />
-                    <link rel="shortcut icon" type="image/png" size="48x48" href="/favicon.png" />
+                    <link rel="shortcut icon" type="image/x-icon" size="48x48" href="/favicon.ico" />
                     <link rel="icon" type="image/png" size="192x192" href="/icon/icon@192.png" />
                     <link rel="apple-touch-icon" type="image/png" size="512x512" href="/icon/icon@512.png" />
                     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Noto+Sans+TC:wght@400;700&display=optional" />
